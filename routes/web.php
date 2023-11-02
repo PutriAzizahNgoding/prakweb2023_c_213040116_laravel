119e7759bda6a143c9ac7c8999461f0f8a2a03af
PutriAzizahNgoding <myputunity@gmail.com>
2023-11-02 11:26:32 +0700

    menambahkan kode lanjutan video ke 5 playlist laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -79,47 +81,10 @@ Route::get('/', function () {
         ]);
      });
 
+Route::get('/posts', [PostController::class,'index']);
 
+Route::get('posts/{slug}',[PostController::class, 'show']);
+   
 
- //halaman single post
- Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-           "title" => "Judul Post Pertama",
-           "slug" => "judul-post-pertama",
-           "author" => "Putri Azizah",
-           "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia cum, amet, eum sunt animi
-            quos quae nisi maiores optio eius ipsa pariatur. Veniam quae sapiente assumenda praesentium a dolorum. 
-            Et est animi eveniet expedita fuga ratione repellat quidem sequi! Quia omnis voluptatum 
-           aperiam a nesciunt cumque fuga maxime repudiandae similique numquam, quam dolorum officiis blanditiis sit facere expedita ipsum veniam pariatur fugit natus quibusdam ad? Dolor corrupti hic odio, laboriosam eveniet labore dignissimos voluptate incidunt
-           consequatur fugit aut optio obcaecati." 
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Mput",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, velit provident, ex 
-            et a nobis qui maiores, optio ducimus enim id iure non! Itaque iste, quam officiis recusandae quisquam 
-            ea saepe deleniti placeat magnam non, et enim eligendi? A saepe laudantium officiis omnis alias eveniet 
-            blanditiis error necessitatibus sed eum quasi, fugiat excepturi porro minus harum iusto voluptas animi 
-            quam commodi. Quas saepe maiores molestias minus et recusandae doloremque tempore, deleniti nihil quibusdam
-            officia nostrum inventore nesciunt, laborum ipsa, itaque quisquam ex? Sint quidem, esse soluta excepturi
-            inventore eum maxime natus culpa labore. Molestias corrupti dolores tempore, ipsum voluptatum quidem?" 
-         ],
-    
-        ];
-
-        $new_post = [];
-        foreach ($blog_posts as $post) {
-            if($post["slug"] === $slug) {
-                $new_post = $post;
-            }
-        }
-
-    return view('post',[
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
- });
 
 
